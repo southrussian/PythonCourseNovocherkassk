61c9baf44ff1c2164be55ca15cce385cc3fb7bc2
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-09-13 11:27:09 +0300

    Added 5th lab. It contains counter of same sentences in two .txt files.

--- Lab_5.py
+++ Lab_5.py
@@ -1,5 +1,24 @@
+import numpy as np
+
+
 def include_substring(path_1: str, path_2: str):
+    counter = 0
     with open(path_1, 'r', encoding='utf-8') as file_1:
-        file_1.read()
+        new_arr_1 = np.array(list(file_1.read()))
+        string_arr_1 = ''.join(new_arr_1)
+        splitted_arr_1 = string_arr_1.split(". ")
     with open(path_2, 'r', encoding='utf-8') as file_2:
-        file_2.read()
+        new_arr_2 = np.array(list(file_2.read()))
+        string_arr_2 = ''.join(new_arr_2)
+        splitted_arr_2 = string_arr_2.split(". ")
+
+    for i in splitted_arr_1:
+        for j in splitted_arr_2:
+            if i == j:
+                counter += 1
+    print(counter)
+    return counter
+
+
+include_substring("/Users/southrussian/PycharmProjects/Laboratories/text_1.txt",
+                  "/Users/southrussian/PycharmProjects/Laboratories/text_2.txt")
