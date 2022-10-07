39470372acc3e960134d96d4b38ae104059e0fa2
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-10-07 12:56:01 +0300

    finished client side

--- Lab_9/client.py
+++ Lab_9/client.py
@@ -27,10 +27,65 @@ class Client:
         receive_thread.start()
 
     def gui_loop(self):
-        pass
+        self.window = tkinter.Tk()
+        self.window.configure(bg="lightgray")
+
+        self.chat_label = tkinter.Label(self.window, text="Chat:", bg="lightgray")
+        self.chat_label.config(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText(self.window)
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.msg_label = tkinter.Label(self.window, text="Chat:", bg="lightgray")
+        self.msg_label.config(font=("Arial", 12))
+        self.msg_label.pack(padx=20, pady=5)
+
+        self.input_area = tkinter.Text(self.window, height=300)
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.window, text="Send", command=self.write)
+        self.send_button.config(font=("Arial", 12))
+        self.send_button.pack(padx=20, pady=5)
+
+        self.gui_done = True
+        self.window.protocol("WM_DELETE_WINDOW", self.stop)
+        self.window.mainloop()
+
+    def write(self):
+        message = f"{self.nickname}: {self.input_area.get('1.0', 'end')}"
+        self.sock.send(message.encode('utf-8'))
+        self.input_area.delete('1.0', 'end')
+
+    def stop(self):
+        self.running = False
+        self.window.destroy()
+        self.sock.close()
+        exit(0)
 
     def receive(self):
-        pass
+        while self.running:
+            try:
+                message = self.sock.recv(1024)
+                if message == "NICK":
+                    self.sock.send(self.nickname.encode('utf-8'))
+                else:
+                    if self.gui_done:
+                        self.text_area.config(state='normal')
+                        self.text_area.insert('end', message)
+                        self.text_area.yview('end')
+                        self.text_area.config('disabled')
+            except ConnectionAbortedError:
+                break
+            except:
+                print("Error")
+                self.sock.close()
+                break
+
+
+client = Client(HOST, PORT)
+
 
 
 
