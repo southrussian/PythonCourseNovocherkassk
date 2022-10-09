7f4f0b9bb168bc7e0667b2073af11225aba3d6c4
Danil Peregorodiev <thekwondoandfootbolfan@gmail.com>
2022-10-09 23:55:33 +0300

    New chat version. Last one is not stable for mac

--- Lab_9/server.py
+++ Lab_9/server.py
@@ -1,55 +1,39 @@
-import socket
-import threading
+from socket import *
+from threading import *
 
-HOST = '127.0.0.1'
-PORT = 9090
-
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen()
-
-clients = []
-nicknames = []
+clients = set()
 
 
-def broadcast(message):
-    for client in clients:
-        client.send(message)
-
-
-def handle(client):
+def client_thread(client_socket, client_address):
     while True:
-        try:
-            message = client.recv(1024)
-            print(f"{nicknames[clients.index(client)]} says {message}")
-            broadcast(message[clients.index(client)])
-        except:
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
+        message = client_socket.recv(1024).decode('utf-8')
+        print(client_address[0] + ":" + str(client_address[1] + " says: " + message))
 
-            nickname = nicknames[index]
-            nicknames.remove(nickname)
+        for client in clients:
+            if client is not client_socket:
+                client.send((client_address[0] + ":" + str(client_address[1]) + " says: " + message).encode('utf-8'))
+
+        if not message:
+            clients.remove(client_socket)
+            print(client_address[0] + ":" + str(client_address[1] + 'disconnected'))
             break
 
-
-def receive():
-    while True:
-        client, address = server.accept()
-        print(f"Connected with {str(address)}")
-        client.send('NICK'.encode('utf-8'))
-        nickname = client.recv(1024)
-
-        nicknames.append(nickname)
-        clients.append(client)
-
-        print(f"Nickname of client is {nickname}")
-        broadcast(f"{nickname} is joined the server\n".encode('utf-8'))
-        client.send("Connected to the server".encode('utf-8'))
-
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
+    client_socket.close()
 
 
-print("Server is running<<")
-receive()
+host_socket = socket(AF_INET, SOCK_STREAM)
+host_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+
+host = '127.0.0.1'
+port = 9090
+host_socket.bind((host, port))
+host_socket.listen()
+
+print('Server is waiting...')
+
+while True:
+    client_socket, client_address = host_socket.accept()
+    clients.add(client_socket)
+    print("Connected with: ", client_address[0] + ':' + str(client_address[1]))
+    thread = Thread(target=client_thread, args=(client_socket, client_address, ))
+    thread.start()
